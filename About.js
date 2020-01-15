9101ba5ea9d4fe6701026a1b4b7ba7a50b1616de
Iqra840 <54781026+Iqra840@users.noreply.github.com>
2020-01-15 18:26:53 +0800

    Add files via upload

---
+++ About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>This is the TodoList app made with React by Iqra Abbasi</p>
+    </React.Fragment>
+  )
+}
+
+
+export default About;
